cb878c21697f12a53fc7306c350eb1250e1f75ee
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-13 06:10:16 +0700

    Completed decide dinner app (small sample)

--- app/src/main/java/vantutrieu97/myandroidtourapplication/MainActivity.kt
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/MainActivity.kt
@@ -1,12 +1,29 @@
 package vantutrieu97.myandroidtourapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
+    val foodList = arrayListOf<String>("Chinese", "Hamburger", "McDonald", "KFC chicken");
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        seletedFoodTxt.text = "Burger King!"
+
+
+        decideBtn.setOnClickListener {
+            val random = Random
+            val randomFoodIndex = random.nextInt(foodList.count())
+            seletedFoodTxt.text = foodList[randomFoodIndex].toString()
+        }
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodEdt.text.toString()
+            foodList.add(newFood)
+            addFoodEdt.text.clear()
+            println(foodList)
+        }
+
     }
 }
