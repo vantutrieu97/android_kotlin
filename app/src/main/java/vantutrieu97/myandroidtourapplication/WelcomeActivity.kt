52b181a34687df1df379e71373fb4e2cf7f36d93
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-15 06:27:53 +0700

    Change screen when click button

--- app/src/main/java/vantutrieu97/myandroidtourapplication/WelcomeActivity.kt
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package vantutrieu97.myandroidtourapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueActivity = Intent(this, LeagueActivity::class.java);
+            startActivity(leagueActivity)
+        }
+
     }
 }
