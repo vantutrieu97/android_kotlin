b892a60a10fa21ae7e398308ca8469fd851f8c90
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-20 11:36:13 +0700

    Add simple listview

--- app/src/main/java/vantutrieu97/myandroidtourapplication/swag/controller/MainSwagActivity.kt
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/swag/controller/MainSwagActivity.kt
@@ -1,13 +1,20 @@
 package vantutrieu97.myandroidtourapplication.swag.controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main_swag.*
 import vantutrieu97.myandroidtourapplication.R
+import vantutrieu97.myandroidtourapplication.swag.model.Category
+import vantutrieu97.myandroidtourapplication.swag.sevices.DataService
 
 class MainSwagActivity : AppCompatActivity() {
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_swag)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/vantutrieu97/myandroidtourapplication/swag/model/Category.kt
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/swag/model/Category.kt
@@ -1,4 +1,35 @@
 package vantutrieu97.myandroidtourapplication.swag.model
 
-class Category constructor(val title: String, val image: String) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Category(val title: String?, val image: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Category> {
+        override fun createFromParcel(parcel: Parcel): Category {
+            return Category(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Category?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun toString(): String {
+        return title.toString()
+    }
 }
