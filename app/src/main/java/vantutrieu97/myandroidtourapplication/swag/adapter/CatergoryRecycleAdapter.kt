7350d3dcf95f0aa5eab069dff3fe562eb8e26f3e
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-24 00:12:20 +0700

    Change listview --> recyclerview

---
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/swag/adapter/CatergoryRecycleAdapter.kt
@@ -0,0 +1,41 @@
+package vantutrieu97.myandroidtourapplication.swag.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import vantutrieu97.myandroidtourapplication.R
+import vantutrieu97.myandroidtourapplication.swag.model.Category
+
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) :
+    RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], context)
+
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView.findViewById<ImageView>(R.id.backgroundImg)
+        val categoryTitle = itemView.findViewById<TextView>(R.id.titleTxt)
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId =
+                context.resources.getIdentifier(category.image, "mipmap", context.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryTitle.text = category.title
+        }
+    }
+}
--- app/src/main/java/vantutrieu97/myandroidtourapplication/swag/controller/MainSwagActivity.kt
+++ app/src/main/java/vantutrieu97/myandroidtourapplication/swag/controller/MainSwagActivity.kt
@@ -2,28 +2,32 @@ package vantutrieu97.myandroidtourapplication.swag.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main_swag.*
 import vantutrieu97.myandroidtourapplication.R
-import vantutrieu97.myandroidtourapplication.swag.adapter.CategoryAdapter
+import vantutrieu97.myandroidtourapplication.swag.adapter.CategoryRecycleAdapter
 import vantutrieu97.myandroidtourapplication.swag.sevices.DataService
 
 class MainSwagActivity : AppCompatActivity() {
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_swag)
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
-        categoryListView.setOnItemClickListener { parent, view, position, id ->
-            run {
-                Toast.makeText(
-                    this,
-                    "You was clicked $position",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
+//        categoryListView.setOnItemClickListener { parent, view, position, id ->
+//            run {
+//                Toast.makeText(
+//                    this,
+//                    "You was clicked $position",
+//                    Toast.LENGTH_LONG
+//                ).show()
+//            }
+//        }
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
